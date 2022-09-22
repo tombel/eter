e3352c5264e5d2ee38aac57e5693fc07ca87ebf5
Rafael Castro <rccastrorafael@gmail.com>
2022-09-22 11:08:00 -0300

    fix: tab size

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	typescript: { tsconfigPath: 'tsconfig.json' },
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: { tsconfigPath: 'tsconfig.json' },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
